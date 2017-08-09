ac806f7207b30b1f4e360ccb749c4ecaa21605e5
Mauricio Collazos <ma0@contraslash.com>
2017-08-09 09:50:09 -0500

    added pep8

--- django_crud_generator.py
+++ django_crud_generator.py
@@ -11,7 +11,10 @@ def sanity_check():
     A valid django app always must have a models.py file
     :return: None
     """
-    if not os.path.isfile(os.path.join(args['django_application_folder'], 'models.py')):
+    if not os.path.isfile(
+        os.path.join(
+            args['django_application_folder'],
+            'models.py')):
         print("django_application_folder is not a Django application folder")
         sys.exit(1)
 
@@ -20,21 +23,40 @@ def conf(args):
     """
     Create or modify the conf.py file
     :param args: command line args
-    :return: 
+    :return:
     """
 
-    if not os.path.isfile(os.path.join(args['django_application_folder'], 'conf.py')):
+    if not os.path.isfile(os.path.join(
+            args['django_application_folder'],
+            'conf.py')):
         # If conf.py does not exists, create
-        conf_file = codecs.open(os.path.join(args['django_application_folder'], 'conf.py'), 'w+', encoding='UTF-8')
+        conf_file = codecs.open(
+            os.path.join(
+                args['django_application_folder'],
+                'conf.py'),
+            'w+',
+            encoding='UTF-8'
+        )
         print("Creating conf.py")
         conf_file.write("from base import conf\n")
     else:
         # If file exists, just load the file
-        conf_file = codecs.open(os.path.join(args['django_application_folder'], 'conf.py'), 'a+', encoding='UTF-8')
+        conf_file = codecs.open(
+            os.path.join(args['django_application_folder'], 'conf.py'),
+            'a+',
+            encoding='UTF-8'
+        )
 
     # Load content from template
-    conf_template = "".join(codecs.open('conf.py.tmpl', encoding='UTF-8').readlines())
-    template_rendered = string.Template(conf_template).safe_substitute(model_prefix=args['model_prefix'])
+    conf_template = "".join(
+        codecs.open(
+            'conf.py.tmpl',
+            encoding='UTF-8'
+        ).readlines()
+    )
+    template_rendered = string.Template(
+        conf_template
+    ).safe_substitute(model_prefix=args['model_prefix'])
 
     # Put content on file
     conf_file.write(template_rendered)
@@ -43,13 +65,22 @@ def conf(args):
 
 def views(args):
     """
-    Create the view file    
-    :param args: command line args 
-    :return: 
+    Create the view file
+    :param args: command line args
+    :return:
     """
-    if not os.path.isdir(os.path.join(args['django_application_folder'], 'views')):
+    if not os.path.isdir(
+        os.path.join(
+            args['django_application_folder'],
+            'views')):
         os.mkdir(os.path.join(args['django_application_folder'], 'views'))
-        codecs.open(os.path.join(args['django_application_folder'], 'views', '__init__.py'), 'w+')
+        codecs.open(
+            os.path.join(
+                args['django_application_folder'],
+                'views',
+                '__init__.py'),
+            'w+'
+        )
 
     view_file = codecs.open(
         os.path.join(
@@ -61,7 +92,12 @@ def views(args):
         encoding='UTF-8'
     )
 
-    view_template = "".join(codecs.open('view.py.tmpl', encoding='UTF-8').readlines())
+    view_template = "".join(
+        codecs.open(
+            'view.py.tmpl',
+            encoding='UTF-8'
+        ).readlines()
+    )
     template_rendered = string.Template(view_template).safe_substitute(
         model_name=args['model_name'],
         model_prefix=args['model_prefix']
@@ -72,23 +108,50 @@ def views(args):
 
 def urls(args):
     """
-    Create or modify the urls_file 
-    :param args: 
-    :return: 
+    Create or modify the urls_file
+    :param args:
+    :return:
     """
 
-    if not os.path.isfile(os.path.join(args['django_application_folder'], 'urls.py')):
+    if not os.path.isfile(
+        os.path.join(
+            args['django_application_folder'],
+            'urls.py')):
         # If conf.py does not exists, create
-        urls_file = codecs.open(os.path.join(args['django_application_folder'], 'urls.py'), 'w+', encoding='UTF-8')
+        urls_file = codecs.open(
+            os.path.join(
+                args['django_application_folder'],
+                'urls.py'
+            ),
+            'w+',
+            encoding='UTF-8'
+        )
         print("Creating urls.py")
-        urls_initial_template = "".join(codecs.open('urls_initial.py.tmpl', encoding='UTF-8').readlines())
+        urls_initial_template = "".join(
+            codecs.open(
+                'urls_initial.py.tmpl',
+                encoding='UTF-8'
+            ).readlines()
+        )
         urls_file.write(urls_initial_template)
     else:
         # If file exists, just load the file
-        urls_file = codecs.open(os.path.join(args['django_application_folder'], 'urls.py'), 'a+', encoding='UTF-8')
+        urls_file = codecs.open(
+            os.path.join(
+                args['django_application_folder'],
+                'urls.py'
+            ),
+            'a+',
+            encoding='UTF-8'
+        )
 
     # Load content from template
-    urls_template = "".join(codecs.open('urls.py.tmpl', encoding='UTF-8').readlines())
+    urls_template = "".join(
+        codecs.open(
+            'urls.py.tmpl',
+            encoding='UTF-8'
+        ).readlines()
+    )
     template_rendered = string.Template(urls_template).safe_substitute(
         model_prefix=args['model_prefix'],
         url_pattern=args['url_pattern'],
@@ -99,20 +162,37 @@ def urls(args):
     urls_file.write(template_rendered)
     urls_file.close()
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser("Create a simple CRUD Structure based contraslash django application structure")
+    parser = argparse.ArgumentParser(
+        "Create a simple CRUD Structure based contraslash django application "
+        "structure"
+    )
 
-    parser.add_argument('--django_application_folder', default=".")
+    parser.add_argument(
+        '--django_application_folder',
+        default="."
+    )
 
-    parser.add_argument('--model_name', type=str, help="Name of model for make the crud", required=True)
+    parser.add_argument(
+        '--model_name',
+        type=str,
+        help="Name of model for make the crud",
+        required=True
+    )
 
-    parser.add_argument('--model_prefix', type=str, help="Prefix name for conf variable")
+    parser.add_argument(
+        '--model_prefix',
+        type=str,
+        help="Prefix name for conf variable"
+    )
 
     parser.add_argument('--url_pattern', type=str, help="Pattern for url")
 
     args = vars(parser.parse_args())
 
-    # If model prefix is not defined, we'll going to define model_prefix as model_name in uppercase
+    # If model prefix is not defined, we'll going to define model_prefix as
+    # model_name in uppercase
     if args['model_prefix'] is None:
         args['model_prefix'] = args['model_name'].upper()
 
@@ -126,7 +206,3 @@ if __name__ == '__main__':
     views(args)
 
     urls(args)
-
-
-
-
