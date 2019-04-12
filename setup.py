27647560008f26c039ffeb69bd2f075a09e0498a
Mauricio Collazos <ma0@contraslash.com>
2019-04-12 12:11:34 -0500

    added _ to ugelazy to use verbose name

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ def readme():
     with codecs.open('README.md') as f:
         return f.read()
 
+
 setup(
     name='django_crud_generator',
-    version='0.3.6',
+    version='0.3.7',
     description='A simple scaffolding for django applications',
     long_description=readme(),
     long_description_content_type="text/markdown",
